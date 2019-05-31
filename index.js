f3de0f9ad18c6f2cea8d951c2b592a9728a873ac
Noah Huppert <contact@noahh.io>
2019-05-31 17:19:35 -0400

    renamed PORT env var to APP_PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const app = express()
 app.use(bodyParser.json())
 
-const port =  process.env.PORT || 3000
+const port =  process.env.APP_PORT || 3000
 
 app.get("/time", (req, res) => {
     res.json({
