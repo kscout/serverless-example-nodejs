43dddc7d93cec55ec6402d415061c37f9ba660c6
Noah Huppert <contact@noahh.io>
2019-05-31 11:11:12 -0400

    wrote example nodejs api

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# serverless-example-nodejs
+# Serverless Example NodeJS
 Example NodeJS serverless application.
+
+# Table Of Contents
+- [Overview](#overview)
+- [Endpoints](#endpoints)
+
+# Overview
+NodeJS express app.
+
+# Endpoints
+HTTP API.  
+GET requests pass data via URL query parameters.  
+All other HTTP methods pass data via JSON encoded bodies.  
+Responses are JSON formatted.  
+
+## `GET /time`
+Request: None
+
+Response:
+
+- `epoch` (Number): Unix time
+
+## `POST /calculate/add`
+Request:
+
+- `lhs` (Number): First number to add
+- `rhs` (Number): Second number to add
+
+Response:
+
+- `result` (Number): Numbers added together
+
+## `GET /health`
+Request: None
+
+Response: 
+
+- `ok` (Boolean): Indicates if server is okay
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(bodyParser.json())
+
+const port =  process.env.PORT || 3000
+
+app.get("/time", (req, res) => {
+    res.json({
+	epoch: Date.now()
+    })
+})
+
+app.post("/calculate/add", (req, res) => {
+    if (!req.body.lhs || !req.body.lhs) {
+	res.status(400).json({ error: "bad request" })
+	return
+    }
+
+    if (isNaN(req.body.lhs) || isNaN(req.body.rhs)) {
+	res.status(400).json({ error: "bad request" })
+	return
+    }
+
+    const lhs = parseInt(req.body.lhs)
+    const rhs = parseInt(req.body.rhs)
+
+    res.json({
+	result: lhs + rhs
+    })
+})
+
+app.get("/health", (req, res) => {
+    res.json({
+	"ok": true
+    })
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
